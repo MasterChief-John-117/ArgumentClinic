f03ebbdc28ad6d89b1a95424c7ef708efcbd5b25
MasterChief-John-117 <MasterChief-John-117@users.noreply.github.com>
2018-09-21 18:11:30 -0700

    First Version

--- Argument.cs
+++ Argument.cs
@@ -1,6 +1,63 @@
-﻿namespace ArgumentClinic
+﻿using System;
+
+namespace ArgumentClinic
 {
-    public class Argument
+    public class Argument<T>
     {
+        private string shortForm;
+        private string longForm;
+        private bool isRequired;
+        private T defaultValue;
+
+        public Argument(string shortArg, bool required = false, T defaultVal = default(T))
+        {
+            this.shortForm = Global.CleanFlag(shortArg);
+            if (this.shortForm.Length != 1)
+            {
+                throw new System.Exception("Short flag must be one alphanumeric character");
+            }
+            this.isRequired = required;
+            this.defaultValue = defaultVal;
+        }
+
+        public Argument(string shortArg, string longArg, bool required = false, T defaultVal = default(T))
+        {
+            this.shortForm = shortArg;
+            if (this.shortForm.Length != 1)
+            {
+                throw new System.Exception("Short flag must be one alphanumeric character");
+            }
+            this.longForm = longArg;
+            this.isRequired = required;
+            this.defaultValue = defaultVal;
+        }
+
+        public T ParseArgs(string[] args)
+        {
+            /* -2 so we go to one before the end of the arguments
+             * The last argument cannot be a flag as there would never be a following value to parse
+             */
+            for(int idx = 0; idx < args.Length - 2; idx++)
+            {
+                string cleanFlag = Global.CleanFlag(args[idx]);
+                if(shortForm == cleanFlag || longForm == cleanFlag)
+                {
+                    try
+                    {
+                        return (T)Convert.ChangeType(args[idx + 1], typeof(T));
+                    }
+                    catch
+                    {
+                        throw new Exception($"Could not parse argument {args[idx]} {args[idx+1]} (Expected type {typeof(T).Name})");
+                    }
+                }
+            }
+
+            if (this.isRequired)
+            {
+                throw new System.Exception($"Argument ({shortForm}|{longForm}) not found");
+            }
+            else return this.defaultValue;
+        }
     }
 }
